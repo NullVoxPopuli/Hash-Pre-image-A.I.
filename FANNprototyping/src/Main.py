73eefc07e9a7f66d928abed884f80e74738fba3b
L. Preston Sego III <pregosego111_prodigy@yahoo.com>
2011-03-14 20:52:36 -0400

    got the network to run.... need to scale back a bit, figure out how to make it let us use binary, instead of it's stupid -1 1 formatting

--- FANNprototyping/src/Main.py
+++ FANNprototyping/src/Main.py
@@ -4,9 +4,9 @@ import hashlib
 def train_network ():
     connection_rate = 1
     learning_rate = 0.7
-    num_input = 2
+    num_input = 512
     num_neurons_hidden = 4
-    num_output = 1
+    num_output = 128
     
     desired_error = 0.0001
     max_iterations = 100000
@@ -17,16 +17,20 @@ def train_network ():
     ann.set_learning_rate(learning_rate)
     ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
     
-    ann.train_on_file("xor.data", max_iterations, iterations_between_reports, desired_error)
+    ann.train_on_file("md5.data", max_iterations, iterations_between_reports, desired_error)
     
-    ann.save("xor_float.net")
+    ann.save("md5_float.net")
 
 
-def test_network (x, y):
+def test_network (input):
     ann = libfann.neural_net()
-    ann.create_from_file("xor_float.net")
-    calc_out = ann.run([x, y])
-    print(calc_out)
+    ann.create_from_file("md5_float.net")
+    ann.set_activation_function_output(libfann.THRESHOLD_SYMMETRIC)
+
+    calc_out = ann.run(input)
+
+    print("the input and the output are " + ( "the same" if (input == calc_out)  else "not the same"))
+
     ann.destroy()
 
 #Make an input file containing the list of values given and their hashed values
@@ -37,47 +41,62 @@ def create_input_file(input):
     for word in input:
         m = hashlib.md5()
         m.update(word)
-        store_hex_bits(m.hexdigest(), f)
-        store_hex_bits(pad_word(word), f);
+        store_hex_as_binary(m.hexdigest(), f)
+        store_hex_as_binary(pad_word(word), f);
     
-def store_hex_bits(val, file):
+def store_hex_as_binary(val, file):
     #Store the hex value as 1's and -1's into the given file
     first_char_written = False;
+    line_to_write = ""
     for char in val:
         if first_char_written:
-            file.write(" ")
+            line_to_write += " "
         if char == '0' or char == '1' or char == '2' or char == '3' or char == '4' or char == '5' or char == '6' or char == '7':
-            file.write("-1")
+            line_to_write +=  "-1"
         else:
-            file.write("1")
+            line_to_write += "1"
         if char == '0' or char == '1' or char == '2' or char == '3' or char == '8' or char == '9' or char == '10' or char == '11':
-            file.write(" -1")
+            line_to_write += " -1"
         else:
-            file.write(" 1")
+            line_to_write += " 1"
         if char == '0' or char == '1' or char == '4' or char == '5' or char == '8' or char == '9' or char == '12' or char == '13':
-            file.write(" -1")
+            line_to_write += " -1"
         else:
-            file.write(" 1")
+            line_to_write += " 1"
         if char == '0' or char == '2' or char == '4' or char == '6' or char == '8' or char == '10' or char == '12' or char == '14':
-            file.write(" -1")
+            line_to_write += " -1"
         else:
-            file.write(" 1")
+            line_to_write += " 1"
         first_char_written = True
+    file.write(line_to_write)
     file.write('\n');
     
-def read_hex_value_string(line):
+#def read_hex_value_string(line):
     #Find the value of the hex value encoded in the string
-    value = ""
-    index = 0
-    chars = line.split(" ")
-    for val in chars:
-        if 
+#    value = ""
+#    index = 0
+#    chars = line.split(" ")
+#    for val in chars:
+#        if 
     
 def pad_word(hexword):
     #Pad the hex input to the hash function with 0's to make it 512 bits long
-    return hexword + ('0' * (32 - len(hexword)))
+    return hexword + ('0' * (128 - len(hexword)))
     
 #create_input_file(['aaa', 'bbb', '111'])
 f = open('md5.data', 'r')
+#for line in f:
+#    read_hex_value_string(line)
+
+train_network()
+lastline = []
+params = True
 for line in f:
-    read_hex_value_string(line)
+    if params:
+        params = False
+        continue
+    lastline = line.split(" ")
+    print(len(lastline))
+
+lastline = [int(num_string) for num_string in lastline]
+test_network(lastline)
