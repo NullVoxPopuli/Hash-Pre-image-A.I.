78160763195ab5f3ae16b07f953d12c594195a71
Kenny Skaggs <skaggskd@GLaDOS.(none)>
2011-03-16 22:07:58 -0400

    made train_network convert given hex to FANN input and altered pad_word to add to the correct side

--- FANNprototyping/src/Main.py
+++ FANNprototyping/src/Main.py
@@ -1,64 +1,81 @@
 from pyfann import libfann
-import hashlib
+from random import randint
 
 def train_network ():
-    connection_rate = 1
-    learning_rate = 0.7
+    connection_rate = .5
+    learning_rate = .7
     num_input = 16
-    num_neurons_hidden = 128
+    num_neurons_hidden = 1500
     num_output = 16
     
     desired_error = 0.0001
-    max_iterations = 100000
-    iterations_between_reports = 1000
+    max_iterations = 1000
+    iterations_between_reports = 100
     
     ann = libfann.neural_net()
     ann.create_sparse_array(connection_rate, (num_input, num_neurons_hidden, num_output))
     ann.set_learning_rate(learning_rate)
-    ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
+    ann.set_activation_function_hidden(libfann.SIGMOID_SYMMETRIC)
+    ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC)
     
-    ann.train_on_file("md5.data", max_iterations, iterations_between_reports, desired_error)
+    ann.train_on_file("sixteen_bit_hexes.data", max_iterations, iterations_between_reports, desired_error)
     
-    ann.save("md5_float.net")
+    print("Connection rate: %f, learning rate: %f, hidden neurons: %f" % (connection_rate, learning_rate, num_neurons_hidden))
+    
+    ann.save("sixteen_float.net")
 
 
 def test_network (input):
     ann = libfann.neural_net()
-    ann.create_from_file("md5_float.net")
+    ann.create_from_file("sixteen_float.net")
     ann.set_activation_function_output(libfann.THRESHOLD_SYMMETRIC)
 
-    calc_out = ann.run(input)
+    calc_out = ann.run(convert_binary_to_FANN_list(pad_word(change_to_binary(hash_to_16_bits(input), 10))))
     
-    print(calc_out)
-    print(change_FANN_to_hex(calc_out))
+    print('ANN gives that you gave: ' + change_FANN_to_hex(calc_out))
     
     
 #    print("the input and the output are " + ( "the same" if (input == calc_out)  else "not the same"))
 
     ann.destroy()
 
-#Make an input file containing the list of values given and their hashed values
-def create_input_file(input):
-    f = open('md5.data', 'w')
-    #Write the header "#of_cases #of_inputs #of_outputs
-    f.write(str(len(input)) + ' 16 16\n')
-    for word in input:
-        hashed_word = hash_to_16_bits(word)
-        print hashed_word
-        fann_binary = convert_binary_to_FANN_format(pad_word(change_to_binary(hashed_word, 10)))
-        f.write(fann_binary + "\n")
+def create_input_file(numberOfValues):
+    f = open('sixteen_bit_hexes.data', 'w')
+    max = 65535
+    i = 0
+    f.write(str(numberOfValues) + ' 16 16\n')
+    while (i < numberOfValues):
+        i += 1
+        value_to_use = hex(randint(0, max))
+        hashed_value = hash_to_16_bits(value_to_use)
+        fann_binary_input = convert_binary_to_FANN_format(pad_word(change_to_binary(hashed_value, 10)))
+        fann_binary_output = convert_binary_to_FANN_format(pad_word(change_to_binary(value_to_use, 16)))
+        f.write(fann_binary_input + '\n')
+        f.write(fann_binary_output + '\n')
         
-        fann_binary = convert_binary_to_FANN_format(pad_word(change_to_binary(word, 16)))
-        f.write(fann_binary + "\n")
+
+#Make an input file containing the list of values given and their hashed values
+#def create_input_file(input):
+#    f = open('md5.data', 'w')
+#    #Write the header "#of_cases #of_inputs #of_outputs
+#    f.write(str(len(input)) + ' 16 16\n')
+#    for word in input:
+#        hashed_word = hash_to_16_bits(word)
+#        print hashed_word
+#        fann_binary = convert_binary_to_FANN_format(pad_word(change_to_binary(hashed_word, 10)))
+#        f.write(fann_binary + "\n")
+#        
+#        fann_binary = convert_binary_to_FANN_format(pad_word(change_to_binary(word, 16)))
+#        f.write(fann_binary + "\n")
 
   
-  #temporary so that we can prove our NN works.  
+#temporary so that we can prove our NN works.  
 def hash_to_16_bits(word):
     return str(hash(word)&65535) #2^16 - 1
     
 def pad_word(bin_word):
     #Pad the hex input to the hash function with 0's to make it 512 bits long
-    return bin_word + ('0' * (16 - len(bin_word)))
+    return ('0' * (16 - len(bin_word))) + bin_word
     
     
 def change_to_binary(original, current_base):
@@ -82,6 +99,15 @@ def convert_binary_to_FANN_format(binary_string):
     binary_string = binary_string.replace("1", "1 ")
     binary_string = binary_string.replace("0", "-1 ")
     return binary_string
+#test_network('AAAA')
+def convert_binary_to_FANN_list(binary_string):
+    list = []
+    for char in binary_string:
+        if (char == '0'):
+            list.append(-1)
+        else:
+            list.append(1)
+    return list
 
 def convert_FANN_format_to_binary(fann_string):
     fann_string = fann_string.replace("-1 ", "0")
@@ -89,9 +115,9 @@ def convert_FANN_format_to_binary(fann_string):
     return fann_string
 
 
-#create_input_file(['aaa', 'bbb', '111'])
+#create_input_file(10000)
 
-#train_network()
+train_network()
 
-#test_network([1, 1, 1, 1, 1, 1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1])
-test_network([1, -1, -1, 1, 1, -1, 1, 1, -1, -1, 1, -1, -1, -1, -1, -1])
+#test_network('1234')
+#test_network([1, -1, -1, 1, 1, -1, 1, 1, -1, -1, 1, -1, -1, -1, -1, -1])
