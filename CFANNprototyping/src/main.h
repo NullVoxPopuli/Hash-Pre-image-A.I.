f56a0b2942b140a078cf34fb467a2be5b01cd7aa
L. Preston Sego III <pregosego111_prodigy@yahoo.com>
2011-03-21 11:20:27 -0400

    cfann now has arguments -test -train and -file

--- CFANNprototyping/src/main.h
+++ CFANNprototyping/src/main.h
@@ -4,6 +4,19 @@
  Contains global constants
  
  */
+
+#define FALSE 0
+#define TRUE 1
+#define  False 0
+#define True 1
+#define false 0
+#define true 1
+
+int OUTPUT_TO_FILE = false;
+int NEED_TO_TRAIN = false;
+int NEED_TO_TEST = false;
+
+
 int hash_width_in_bits = 8;
 
 void train_network();
