8e9d33579dde51e1025b4228934d9f26bdb11af5
NullVoxPopuli <LPSego3+dev@gmail.com>
2013-04-06 15:34:18 -0400

    how do I convert string to binary? lol

--- RubyPlayground/md5.rb
+++ RubyPlayground/md5.rb
@@ -16,18 +16,6 @@ samples = [
 ruby_md5s = samples.map{ | e | Digest::MD5.hexdigest( e ) }
 
 # now implement it, and see if we come up with the same answer
-# ported over from python:
-# http://rosettacode.org/wiki/MD5/Implementation#Python
-rotate_amounts = [ 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-                   5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
-                   4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-                   6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21 ]
-
-constants = []
-64.times { | i | constants << (Math.sin( i + 1 ).abs * ( 2 ** 32 )).floor }
-
-initial_values = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
-
 def left_rotate(x, amount)
 	x &= 0xFFFFFFFF
 	return (( x << amount ) | (x >> (32 - amount ))) & 0xFFFFFFFF
@@ -36,12 +24,25 @@ end
 # based off http://en.wikipedia.org/wiki/MD5
 # and http://rosettacode.org/wiki/MD5/Implementation#Python
 def md5(msg)
+	rotate_amounts = [ 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+	                   5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+	                   4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+	                   6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21 ]
+
+	constants = []
+	64.times { | i | constants << (Math.sin( i + 1 ).abs * ( 2 ** 32 )).floor }
+
+	initial_values = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
+
+
+
+
 	# Pre-processing: adding a single 1 bit
-	msg + 0b1
+	msg << 0b1
 
 	# Pre-processing: padding with zeros
 	while ((msg.bytesize * 8) % 512) < 448
-		msg += 0b0
+		msg << 0b0
 	end
 
 	# Process the message in successive 512-bit chunks
@@ -51,8 +52,8 @@ def md5(msg)
 
 		# break chunk into 32 bitwords (4 is bytes)
 		m = []
-		bit_chunk_512.each_slice( 4 ).each_slice { | bit_chunk_32 |
-			m << bit_chunk_32
+		bit_chunk_512.each_slice( 4 ) { | bit_chunk_32 |
+			m << bit_chunk_32.join
 		}
 
 		a, b, c, d = initial_values
@@ -63,25 +64,22 @@ def md5(msg)
 			f = 0
 			g = 0
 
-			if 0 <= i <= 15
-				f = (b and c) or (!b and d)
+			if 0 <= i and i <= 15
+				f = ( b & c ) | ( ~b & d )
 				g = i
-			elsif 16 <= i <= 31
-				f = (d and b) or (!d and c)
-				g = (5 * i + 1) % 16
-			elsif 32 <= i <= 47
+			elsif 16 <= i and i <= 31
+				f = ( d & b ) | ( ~d & c )
+				g = ( 5 * i + 1 ) % 16
+			elsif 32 <= i and i <= 47
 				f = b ^ c ^ d
-				g = (3 * i + 5) % 16
-			elsif 48 <= i <= 63
-				f = c ^ (b or !d)
-				g = (7 * i)  % 16
+				g = ( 3 * i + 5 ) % 16
+			elsif 48 <= i and i <= 63
+				f = c ^ ( b | ~d )
+				g = ( 7 * i )  % 16
 			end
 
-			d_temp = d
-			d = c
-			c = b
-			b = b + left_rotate((a +f + constants[i] + m[g]), rotate_amounts[i])
-			a = d_temp
+			a, c, d = d, b, c
+			b = b + left_rotate((a + f + constants[i] + m[g]), rotate_amounts[i])
 
 		}
 
